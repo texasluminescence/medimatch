de8b0221edf59bc025e82b49de70d22e99db890e
Pranav Belligundu <pranav.belligundu@gmail.com>
2024-12-01 16:51:26 -0600

    working hugging face

--- lib/test.py
+++ lib/test.py
@@ -1,21 +1,16 @@
-import joblib
-import sklearn
+from transformers import pipeline
 
-# Load the saved encoders
-encoders = joblib.load('../lib/encoders.pkl')
+# Load the model
+classifier = pipeline("text-classification", model="Zabihin/Symptom_to_Diagnosis", tokenizer="Zabihin/Symptom_to_Diagnosis")
 
-# Function to encode new user inputs using loaded encoders
-def encode_input(user_input, encoders):
-    encoded_input = {}
-    for feature, value in user_input.items():
-        if feature in encoders:
-            encoded_input[feature] = encoders[feature].transform([value])[0]
-        else:
-            encoded_input[feature] = value  # For features that don't need encoding
-    return encoded_input
+# Example input text
+input_text = "coughing and fever"
 
-# Example user input
-user_input = {'Fever': 'Yes', 'Cough': 'No', 'Gender': 'Male', 'Age': 35}  # Assuming 'Age' doesn't need encoding
+# Get the predicted label
+result = classifier(input_text)
 
-# Encode the input
-encoded_input = encode_input(user_input, encoders)
+# Print the predicted label
+predicted_label = result[0]['label']
+print("Predicted Label:", predicted_label)
+
+#citation: https://huggingface.co/Zabihin/Symptom_to_Diagnosis
